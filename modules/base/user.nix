55eea1656c4bada5b2778f1c7a668de10e74ad40
Rubikoid <rubikoid@rubikoid.ru>
2025-01-02 20:52:01 +0300

    Add user.nix

---
+++ modules/base/user.nix
@@ -0,0 +1,23 @@
+{
+  inputs,
+  pkgs,
+  lib,
+  config,
+  secrets,
+  device,
+  mode,
+  ...
+}:
+
+{
+  # programs.home-manager.enable = true; # idk why i need that
+  home =
+    let
+      baseHomePath = if lib.hasPrefix "Darwin" mode then "/Users" else "/home";
+    in
+    {
+      username = config.user;
+      homeDirectory = "${baseHomePath}/${config.user}";
+      stateVersion = "24.05";
+    };
+}
