a6686ac17f626e2bf42f2a381e006a95430d414d
Rubikoid <rubikoid@rubikoid.ru>
2025-01-19 22:26:55 +0300

    Default proxmox setup

--- modules/system/cloud/proxmox-lxc.nix
+++ modules/system/cloud/proxmox-lxc.nix
@@ -1,7 +1,13 @@
-{ modulesPath, ... }:
+{ modulesPath, lib, ... }:
 
 {
   imports = [
     "${toString modulesPath}/virtualisation/proxmox-lxc.nix"
   ];
+
+  proxmoxLXC = {
+    manageNetwork = lib.mkDefault false;
+    privileged = lib.mkDefault false;
+    manageHostName = lib.mkDefault true;
+  };
 }
