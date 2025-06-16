adcbec44fa4b3c6a20ee6ef84b5904960090ee93
Rubikoid <rubikoid@rubikoid.ru>
2025-06-16 16:21:34 +0300

    Update patch to nixfmt

--- pkgs/nixfmt-patched.nix
+++ pkgs/nixfmt-patched.nix
@@ -6,7 +6,7 @@
 let
   patch = pkgs.fetchpatch2 {
     url = "https://github.com/Rubikoid/nixfmt/pull/1.patch";
-    sha256 = "sha256-0x9VBFuQtc36g4LSRZanb/9XJ6fni6sCiWQyxue8pog=";
+    sha256 = "sha256-YixtONu7Vc6vI6CMZXRKGU/p4s0d75bjfuQDemAhbQ8="; # "sha256-0x9VBFuQtc36g4LSRZanb/9XJ6fni6sCiWQyxue8pog=";
   };
 in
 pkgs.nixfmt-rfc-style.overrideAttrs (old: {
