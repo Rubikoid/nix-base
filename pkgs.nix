4e37d0d19c96b4110848b735ed9c0ca2e2dd99c4
Rubikoid <rubikoid@rubikoid.ru>
2025-08-18 04:22:42 +0300

    Move octodns-selectel from rubikoid/infra

--- pkgs.nix
+++ pkgs.nix
@@ -1,3 +1,4 @@
 inputs: final: prev: rec {
   nixfmt-rubi-style = final.callPackage ./pkgs/nixfmt-patched.nix { };
+  octodns-selectel = final.python312Packages.callPackage ./pkgs/octodns-selectel.nix { };
 }
